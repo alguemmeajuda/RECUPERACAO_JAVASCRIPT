4b7f2129f1cdb2369b3258173a78b348fdc31f24
alguemmeajuda <124810553+alguemmeajuda@users.noreply.github.com>
2023-12-07 18:55:34 -0300

    Update CoordenadasDeUmPonto.js

--- CoordenadasDeUmPonto.js
+++ CoordenadasDeUmPonto.js
@@ -1,20 +1,10 @@
 function ponto(){
     var x,y,p;
-    var Q1,Q2,Q3,Q4;
     x=4.5;
-    p=-2.2;
+    y=-2.2;
     
-    if (x>0){
-        p=Q1||Q4;
-}else{
-        p=Q2||Q3;
-    }
-
-    if (y>0){
-        p=Q2||Q1;
-    }else{
-        p=Q3||Q4;
-    }
-
-alert(p);
+    if (x>0&&y<0){
+        p=Q4;
+   }
+ alert(p);
 }
